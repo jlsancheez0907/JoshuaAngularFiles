0380810898646d2b47f1f3e472ee70a2ba5892f7
jlsancheez0907 <36814869+jlsancheez0907@users.noreply.github.com>
2018-02-25 20:56:46 +0800

    angular files
    pushe

---
+++ APM-Start/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `<div> <h1>{{pageTitle}}</h1>
+  <div> <pm-products></pm-products></div>
+  </div>`
+})
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
+}
