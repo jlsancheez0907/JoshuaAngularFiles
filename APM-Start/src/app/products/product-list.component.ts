7b782efbdcc75309b62e021c932c72149d1b0f25
jlsancheez0907 <36814869+jlsancheez0907@users.noreply.github.com>
2018-03-01 01:21:52 +0800

    a
    a

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls:['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+    ngOnInit(): void {
+        console.log('on initialize');
+    }
     pageTitle: string ='Product List';
     imageWidth: number =50;
     imageMargin: number=2;
     showImage: boolean = false;
-    listFilter: string =  'cart';
+
+    _listFilter:string;
+    get listFilter():string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter = value;
+        this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
+    
     products: IProduct[] = [
         {
             "productId": 1,
@@ -64,6 +79,14 @@ export class ProductListComponent {
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
         }
     ];
+    constructor (){
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+    performFilter(filterBy:string):IProduct []{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy)  !== -1);
+    }   
     toggleImage(): void{
         this.showImage = !this.showImage;
     }
