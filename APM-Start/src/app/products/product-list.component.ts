d1d65680e7998f1c319144fd03ebabe02d07aac2
jlsancheez0907 <36814869+jlsancheez0907@users.noreply.github.com>
2018-03-06 00:07:20 +0800

    latest march 6 2018
    latest march 6 2018

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -90,4 +90,7 @@ export class ProductListComponent implements OnInit {
     toggleImage(): void{
         this.showImage = !this.showImage;
     }
+    onRatingClicked(message:string):void{
+        this.pageTitle = 'Product List' + message;
+    }
 }
