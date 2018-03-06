13057c60bea37e99a310da95bf8c875bbab9f91a
jlsancheez0907 <36814869+jlsancheez0907@users.noreply.github.com>
2018-03-07 01:01:27 +0800

    commit service
    asd

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces-pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductService } from './products/product.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule
-    
+    FormsModule,
+    HttpClientModule 
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
